36b540ab070f551c0a77f274926099cce114ef8e
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-20 10:24:11 +0100

    Add person class

---
+++ person.rb
@@ -0,0 +1,29 @@
+# Person class
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # getters
+  attr_reader :id
+
+  attr_accessor :name, :age
+
+  # setters
+
+  # private method
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
