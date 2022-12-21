1bccf688f0a9a8cff1099687afcb1e63d78aad1b
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-21 22:52:06 +0100

    Update Person class

--- person.rb
+++ person.rb
@@ -1,6 +1,11 @@
+require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
 # Person class
-class Person
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -26,4 +31,15 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+puts capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+puts capitalized_trimmed_person.correct_name
