6f1dc12047b446babca90915f52dd1e73cb7bfc7
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-23 10:53:29 +0100

    Update Student class

--- student.rb
+++ student.rb
@@ -7,6 +7,13 @@ class Student < Person
     @classroom = classroom
   end
 
+  attr_reader :classroom
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
   def play_hooky
     '¯(ツ)/¯'
   end
