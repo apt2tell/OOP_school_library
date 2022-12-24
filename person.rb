c727531c956399ed3a0c8e771cfefcc3663e5466
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-24 11:52:57 +0100

    Update Person class

--- person.rb
+++ person.rb
@@ -46,5 +46,5 @@ person = Person.new(22, 'maximilianus')
 puts person.correct_name
 puts capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
-puts capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
