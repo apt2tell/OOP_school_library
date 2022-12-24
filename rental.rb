24c612161748c6d0f88d72a4993c5d79247ebba6
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-24 11:53:17 +0100

    Update rental.rb file

--- rental.rb
+++ rental.rb
@@ -5,8 +5,8 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    @person = person
     book.rentals << self
+    @person = person
     person.rentals << self
   end
 end
