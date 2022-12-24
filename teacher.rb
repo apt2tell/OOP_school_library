db1fdc8ceaf165f468b96ddd17cfbb91684d7d2a
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-24 15:20:04 +0100

    Update Teacher class

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 # Teacher class
 class Teacher < Person
-  def initialize(specialization, name = 'Unknown')
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
